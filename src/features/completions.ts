684120c9206099de0c4b216c83246e1d11dd6d7e
Mocuto Oshi <xmocuto@aim.com>
2016-11-07 11:25:24 -0500

    Added CompletionItemKind to AutoComplete Added CompletionItemDetail to AutoComplete ISSUE: completion.typeInfo.declAs is undefined, which inhibits us from showing special symbols based on the symbol's type

--- src/features/completions.ts
+++ src/features/completions.ts
@@ -2,8 +2,38 @@ import * as vscode from 'vscode'
 import {instanceManager} from '../extension'
 import logapi = require("loglevel")
 import * as utils from "../utils"
+import * as ensimeClient from "ensime-client"
 
 const log = logapi.getLogger('ensime.completions')
+
+function determineItemKind(completion : ensimeClient.serverProtocol.Completion) : vscode.CompletionItemKind | undefined {
+    var kind = undefined
+    if(completion.typeInfo)
+    {
+        switch(completion.typeInfo.declAs)
+        {
+            case ensimeClient.serverProtocol.DeclaredAs.Class:
+                kind = vscode.CompletionItemKind.Class
+                break
+            case ensimeClient.serverProtocol.DeclaredAs.Field:
+                kind = vscode.CompletionItemKind.Field
+                break
+            case ensimeClient.serverProtocol.DeclaredAs.Interface:
+                kind = vscode.CompletionItemKind.Interface
+                break
+            case ensimeClient.serverProtocol.DeclaredAs.Method:
+                kind = vscode.CompletionItemKind.Method
+                break
+            case ensimeClient.serverProtocol.DeclaredAs.Nil:
+                kind = vscode.CompletionItemKind.Value
+            case ensimeClient.serverProtocol.DeclaredAs.Object:
+                kind = vscode.CompletionItemKind.Module
+            case ensimeClient.serverProtocol.DeclaredAs.Trait:
+                kind = vscode.CompletionItemKind.Interface
+        }
+    }
+    return kind
+}
 
 export function completionsProvider()  {
 
@@ -18,9 +48,16 @@ export function completionsProvider()  {
                 return instance.api.getCompletions(utils.getFilenameDriveUpper(document), document.getText(), document.offsetAt(position), 30).then(response => {
                     log.debug('completions received: ', response.completions)
                     const completions = response.completions.map(completion => {
-                        const toInsert =
+                        log.debug(completion)
+                        let detail =
+                            completion.typeInfo ? completion.typeInfo.fullName : ""
+                        let toInsert =
                             completion.toInsert ? completion.toInsert : completion.name
-                        return new vscode.CompletionItem(toInsert)
+
+                        let c = new vscode.CompletionItem(toInsert)
+                        c.detail = detail
+                        c.kind = determineItemKind(completion)
+                        return c
                     })
                     return new vscode.CompletionList(completions, true)
                 })
